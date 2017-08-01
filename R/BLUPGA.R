b0bf8baa4ef3e7a837b30c9754f5e751a2cfd5bf
dkainer <david.kainer@anu.edu.au>
2017-08-01 12:51:47 +1000

    split the source into primary BLUPGA methods and helper methods.

--- R/BLUPGA.R
+++ R/BLUPGA.R
@@ -1,18 +1,44 @@
 devtools::use_package("cpgen")
 devtools::use_package("rrBLUP")
 
-#' Standard GBLUP
+#' Basic BLUP|GA
 #'
 #' This function runs the BLUP|GA method where certain SNPs are weighted in the a special GRM (S) prior to GBLUP.
-#' @param G GRM constructed using all available SNPs and all samples Defaults to NULL. Use \code{make_GRM()} to get this.
-#' @param S Weighted G-matrix constructed using only selected SNPs and all samples Defaults to NULL. Use \code{make_weighted_GRM()} to get this.
+#' @param G GRM constructed using all available SNPs and all samples Defaults to NULL. Use \code{\link{make_GRM}} to get this.
+#' @param S Weighted G-matrix constructed using only selected SNPs and all samples Defaults to NULL. Use \code{\link{make_weighted_GRM}} to get this.
 #' @param phenodata data frame with 2 columns. One col must be named 'ID' and contain sample IDs. Another column must be named 'y' and contain the phenotypes. Defaults to NULL.
 #' @param valset vector of indices that defines which rows in \code{phenodata} will be set to NA and used for cross-validation Defaults to NULL.
-#' @return a data frame containing the correlation between the predicted phenotype and the true phenotype of the individuals in the \code{valset}. Each row contains that correlation at increasing values of the weighting value W.
+#' @return A data frame containing the correlation between the predicted phenotype and the true phenotype of the individuals in the \code{valset}.
+#'
+#'  Since BLUP|GA is run for each value of omega (W) from 0.0 to 1.0 in increments of 0.10, each row of the returned data frame
+#'   contains the cross-validation correlation at one value of omega (W). This allows the user to find the value of W
+#'   at which the predictive ability (COR) is maximised.
+#'
+#'   Columns:
+#' \describe{
+#'   \item{W}{omega weighting for selected SNPS in candidate genes (0.0--1.0)}
+#'   \item{COR}{cross validation predictive ability (0.0--1.0)}
+#'   }
 #' @keywords GBLUP,BLUP|GA,SNP selection
 #' @export
 #' @examples
-#' blupga()
+#' # get example genotype matrix and phenotype data
+#' data(M)
+#' data(pheno)
+#'
+#' # select some 'special' SNPs from M to be weighted
+#' GAsnps <- sample(1:ncol(M), 20)
+#'
+#' # generate random weights for the 'special' SNPs
+#' wt <- runif(length(GAsnps), min = 1, max = 10)
+#' # make a weighted GRM for the 'special' SNPs
+#' S <- make_weighted_GRM(M[,GAsnps], wt)
+#' # make a standard GRM for all SNPs
+#' G <- make_GRM(M)
+#'
+#' # choose a validation set of 20 random individuals
+#' val <- sample(1:nrow(pheno), 20)
+#' results <- blupga(G, S, pheno, val)
 blupga <- function(G, S, phenodata, valset, verbose=T)
 {
   if (verbose==T) cat("POS\n")
@@ -41,10 +67,23 @@ blupga <- function(G, S, phenodata, valset, verbose=T)
 #' @param G GRM constructed using all available SNPs and all samples Defaults to NULL. Use \code{make_GRM()} to get this.
 #' @param phenodata data frame with 2 columns. One col must be named 'ID' and contain sample IDs. Another column must be named 'y' and contain the phenotypes. Defaults to NULL.
 #' @param valset vector of indices that defines which rows in \code{phenodata} will be set to NA and used for cross-validation Defaults to NULL.
+#' @return a data frame containing the correlation between the predicted phenotype and the true phenotype of the individuals in the valset.
+#' \describe{
+#'   \item{W}{omega weighting for selected SNPS in candidate genes (0.0--1.0)}
+#'   \item{COR}{cross validation predictive ability (0.0--1.0)}
+#'   }
 #' @keywords GBLUP,BLUP|GA,SNP selection
 #' @export
 #' @examples
-#' blupga_GBLUP()
+#' # get example genotype matrix and phenotype data
+#' data(M)
+#' data(pheno)
+#'
+#' G <- make_GRM(M)
+#'
+#' # choose a validation set of 20 random individuals
+#' val <- sample(1:nrow(pheno), 20)
+#' results <- blupga_GBLUP(G, pheno, val)
 blupga_GBLUP <- function(G, phenodata, valset, verbose=T)
 {
   if (verbose==T) cat("GBLUP\n")
@@ -66,11 +105,20 @@ blupga_GBLUP <- function(G, phenodata, valset, verbose=T)
 #' @param phenodata data frame with 2 columns. One col must be named 'ID' and contain sample IDs. Another column must be named 'y' and contain the phenotypes. Defaults to NULL.
 #' @param valset vector of indices that defines which rows in \code{phenodata} will be set to NA and used for cross-validation Defaults to NULL.
 #' @param GRMs List of G-matrices, each constructed from just the SNPs in one candidate gene/region. Defaults to NULL.
-#' @return a data frame containing the correlation between the predicted phenotype and the true phenotype of the individuals in the \code{valset}. Each row contains that correlation at increasing values of the weighting value W.
+#' @return A data frame containing the correlation between the predicted phenotype and the true phenotype of the individuals in the \code{valset}.
+#'
+#'  Since BLUP|GA is run for each value of omega (W) from 0.0 to 1.0 in increments of 0.10, each row of the returned data frame
+#'   contains the cross-validation correlation at one value of omega (W). This allows the user to find the value of W
+#'   at which the predictive ability (COR) is maximised.
+#' \describe{
+#'   \item{W}{omega weighting for selected SNPS in candidate genes (0.0--1.0)}
+#'   \item{COR}{cross validation predictive ability (0.0--1.0)}
+#'   }
 #' @keywords GBLUP,BLUP|GA,SNP selection
 #' @export
 #' @examples
 #' blupga_CAND()
+
 blupga_CAND <- function(G=NULL, phenodata=NULL, valset=NULL, GRMs=NULL, verbose=T)
 {
   if (verbose==T) cat("CAND\n")
@@ -136,11 +184,40 @@ blupga_CAND <- function(G=NULL, phenodata=NULL, valset=NULL, GRMs=NULL, verbose=
 #' @param perc proportion of SNPs to be weighted (between 0 and 1), where 0.05 means the top 5 percent of SNPs will be weighted. Defaults to NULL.
 #' @param flank choose to include the immediate SNPs to the left and right of a top 'perc' SNP. Defaults to TRUE.
 #' @param verbose just leave this for now!
-#' @return a data frame containing the correlation between the predicted phenotype and the true phenotype of the individuals in the \code{valset}. Each row contains that correlation at increasing values of the weighting value W.
+#' @return A data frame containing the correlation between the predicted phenotype and the true phenotype of the individuals in the \code{valset}.
+#'
+#'  Since BLUP|GA is run for each value of omega (W) from 0.0 to 1.0 in increments of 0.10, each row of the returned data frame
+#'   contains the cross-validation correlation at one value of omega (W). This allows the user to find the value of W
+#'   at which the predictive ability (COR) is maximised.
+#' \describe{
+#'   \item{W}{omega weighting for selected SNPS in candidate genes (0.0--1.0)}
+#'   \item{COR}{cross validation predictive ability (0.0--1.0)}
+#'   }
 #' @keywords GBLUP,BLUP|GA,SNP selection
 #' @export
 #' @examples
-#' blupga_EFF()
+#' # get example genotype matrix and phenotype data
+#' data(M)
+#' data(pheno)
+#'
+#'
+#' # choose a validation set of 20 random individuals
+#' val <- sample(1:nrow(pheno), 20)
+#'
+#' # Run a GWAS to estimate squared SNP effects for all SNPs.
+#' # By calling est_SNPeffects() this process will be done using the individuals NOT in your 'val' set.
+#' # Of course you can use your own method to generate the full vector of SNP effects using a training set of individuals.
+#' bsq <- est_SNPeffects(pheno, M, val)
+#'
+#' # make a standard GRM for all SNPs
+#' G <- make_GRM(M)
+#'
+#' # run BLUPGA where the top 1 percent of SNPs (according to effect size) are weighted
+#' results <- blupga_EFF(G, pheno, val, M, bsq, 0.01, flank=FALSE)
+#'
+#' # run BLUPGA where the top 0.1 percent of SNPs (according to effect size) are weighted and
+#' # SNPs immediately upstream and downstream of those top SNPs are also weighted.
+#' results <- blupga_EFF(G, pheno, val, M, bsq, 0.001, flank=TRUE)
 blupga_EFF <- function(G, phenodata, valset=NULL, genomat, bsq, perc, flank=TRUE, verbose=TRUE)
 {
   if (verbose==T) cat("EFF\n")
--- R/helpers.R
+++ R/helpers.R
@@ -5,11 +5,16 @@
 #' This function ...
 #' @param genomat matrix of genotypes in -1,0,1 format (i.e. 0 is heterozygous, 1 and -1 are opposing homozygous states)
 #' @param weights vector of weights for each of the SNPs in the \code{genomat}. Use a vector of 1s to avoid weighting.
-#' @return a weighted Genomic Relationship Matrix with cols and rows named according to the rownames in \code{genomat}. Uses the 'cpgen' package implementation of Van Radens GRM method
+#' @return a weighted Genomic Relationship Matrix with cols and rows named according to the rownames in \code{genomat}. Uses the \pkg{cpgen} package implementation of Van Radens GRM method
 #' @keywords GBLUP,BLUP|GA,SNP selection
 #' @export
 #' @examples
-#' make_weighted_GRM()
+#' # get an example genotype matrix
+#' data(M)
+#' # generate weights for the SNPs
+#' wt <- runif(ncol(M), min = 1, max = 10)
+#'
+#' make_weighted_GRM(M, wt)
 make_weighted_GRM <- function(genomat, weights)
 {
   G1 <- cpgen::cgrm(genomat, weights) #uses the fast, low mem cpgen implementation of Van Raden (2008).
@@ -22,11 +27,13 @@ make_weighted_GRM <- function(genomat, weights)
 #'
 #' This function ....
 #' @param genomat matrix of genotypes in -1,0,1 format (i.e. 0 is heterozygous, 1 and -1 are opposing homozygous states)
-#' @return additive Genomic Relationship Matrix with cols and rows named according to the rownames in \code{genomat}. Uses the 'cpgen' package implementation of Van Radens GRM method
+#' @return additive Genomic Relationship Matrix with cols and rows named according to the rownames in \code{genomat}.Uses the \pkg{cpgen} package implementation of Van Radens GRM method
 #' @keywords GBLUP,BLUP|GA,SNP selection
 #' @export
 #' @examples
-#' make_GRM()
+#' # get an example genotype matrix
+#' data(M)
+#' make_GRM(M)
 make_GRM <- function(genomat)
 {
   G1 <- cpgen::cgrm.A(genomat) #uses the fast, low mem cpgen implementation of Van Raden (2008).
@@ -41,10 +48,16 @@ make_GRM <- function(genomat)
 #' @param genomat matrix of genotypes in -1,0,1 format (i.e. 0 is heterozygous, 1 and -1 are opposing homozygous states)
 #' @param phenodata data frame with 2 columns. One col must be named 'ID' and contain sample IDs. Another column must be named 'y' and contain the phenotypes. Defaults to NULL.
 #' @param valset vector of indices Defaults to NULL.
-#' @return a vector of squared SNP effects, one per SNP in \code{genomat}, estimated with the EMMAX GWAS method from the 'cpgen' package.
+#' @return a vector of squared SNP effects, one per SNP in \code{genomat}, estimated with the EMMAX GWAS method from the \pkg{cpgen} package.
 #' @export
 #' @examples
-#' est_SNPeffects()
+#' # get an example genotype matrix
+#' data(M)
+#' # get an example phenotype data frame
+#' data(pheno)
+#' # choose a validation set of 20 random individuals
+#' val <- sample(1:nrow(pheno), 20)
+#' est_SNPeffects(pheno, M, val)
 est_SNPeffects <- function(phenodata, genomat, valset, fixmat=NULL)
 {
   cat("estimating marker effects with EMMAX\n")
